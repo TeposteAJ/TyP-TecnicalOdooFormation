7ce1748f4f5e56e1441234a3355cac9e8f93eb0b
AngelaTeposte <teposte.villalpa@gmail.com>
2023-03-03 15:36:09 -0700

    [ADD] Basics Module Library

---
+++ odoo_library/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Biblioteca',
+    
+    'sumary': """ Bilioteca App to manage .. """,
+    
+    'description': """
+        Biblioteca Module to mange ... 
+        - Courses
+        - Sessions
+        - Attendes
+    """,
+    
+    'author': 'Angela Tesposte',
+    
+    'website': 'none',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+     
+        
+    ],
+    
+    'demo': [
+    
+        
+    ],
+}
