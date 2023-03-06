5dd4acd42f93e23c668145579e4e9d10d3ab738c
TeposteAJ <odoo@ampd02.odoo.com>
2023-03-06 19:19:37 +0000

    [ADD] Basic Module

---
+++ spacial_mision/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Space Mision',
+    'version': '1.0',
+    'summary': """ Spacial Mision App to manage the mision to the space""",
+    # 'description': """
+    #     Library App to manage books and clients, that need help to rent books.
+    #     - Clients
+    #     -Books
+    # """,
+    'author': 'Angela Tesposte',
+    'website': 'https://github.com/TeposteAJ',
+    'license': 'GPL-3',
+    'category': 'Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable':True,
+    'auto_install': False,
+    'aplication': True,
+}
